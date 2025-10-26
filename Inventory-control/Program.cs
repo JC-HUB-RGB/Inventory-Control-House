769f319f45462734f521368d91ac7debc4bb661b
JC Uzui <cokeklo.delarosa@gmail.com>
2025-10-26 17:57:55 -0600

    Primer Formato he inclusion de la libreria CloseXML

---
+++ Inventory-control/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Inventory Control System!");
+Console.WriteLine("Enter 1 to add an item, 2 to view inventory, or 3 to exit.");
+string input = Console.ReadLine();
